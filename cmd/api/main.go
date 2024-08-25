06284d65d257790a3fd6eb45a22c896aec3164f7
Madalin Popa <contact@madalinpopa.com>
2024-08-25 15:02:32 +0300

    chore: Remove commented code and unused server variable

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,22 +1,19 @@
 package main
 
-import (
-	"log"
-	"net/http"
-
-	"github.com/madalinpopa/loto/internal/api"
-)
+import "github.com/madalinpopa/loto/internal/api"
 
 func main() {
 
 	// Setup a new Mux Server and its routes
-	mux := api.SetupRoutes()
+	// mux := api.SetupRoutes()
 
-	server := &http.Server{
-		Addr:    ":8000",
-		Handler: mux,
-	}
+	// server := &http.Server{
+	// 	Addr:    ":8000",
+	// 	Handler: mux,
+	// }
 
-	log.Println("Server starting on :8000")
-	log.Fatal(server.ListenAndServe())
+	// log.Println("Server starting on :8000")
+	// log.Fatal(server.ListenAndServe())
+
+	api.SetupRoutes()
 }
